444656382ea621e26f25fe798dba521470b1e2f7
Mohan <smohan73866@gmail.com>
2025-08-08 16:47:35 +0530

    food app

--- foodapp1/src/main/java/com/util/DBConnection.java
+++ foodapp1/src/main/java/com/util/DBConnection.java
@@ -6,9 +6,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	static String url="jdbc:mysql://localhost:3306/foodapp";
-	static String user="root";
-	static String password="Mohan@123";
+	static String url=System.getenv("jdbc:mysql://mysql-mohan.alwaysdata.net:3306/foodapp");
+	static String user=System.getenv("mohan_foodapp");
+	static String password=System.getenv("Vijay@123#");
 	static Connection con;
 	public static Connection getConnection()
 	{
